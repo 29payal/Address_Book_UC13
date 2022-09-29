1bdfd2c050c59a3d67cbe85e2d5f6ff80ab2e108
Payal Sharma <https://github.com/29payal>
2022-09-29 17:27:17 +0530

    [Payal] Address-Book-UC13

---
+++ File IO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadOrWrite readOrWrite = new ReadOrWrite();
+            string path = @"C:\Users\DELL\Desktop\RFP\Address_Book_UC13\File IO\Demo.txt";
+            readOrWrite.Read(path);
+            readOrWrite.Write(path);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ File IO/ReadOrWrite.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO
+{
+    public class ReadOrWrite
+    {
+       // string path = @"C:\Users\DELL\Desktop\RFP\Address_Book_UC13\File IO\Demo.txt";
+        public void Read(string path)
+        {
+            using (StreamReader sw = File.OpenText(path))
+            {
+                string line;
+                while((line = sw.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+        }
+        public void Write(string path)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("FirstName   : Payal");
+                sw.WriteLine("LastName    : Sharma");
+                sw.WriteLine("Address     : 12 streets");
+                sw.WriteLine("PhoneNumber : 9087865433");
+                sw.WriteLine("City        : Nimuch");
+                sw.WriteLine("State       : Madhya Pradesh");
+                sw.WriteLine("Zip         : 458441");
+                sw.WriteLine("Email       : abc@gmail.com"); 
+                    sw.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+    }
+}
